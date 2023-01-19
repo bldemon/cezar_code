849e2f1e899f71ccfe948585edd58582bd65518a
bldemon <bldemon@yandex.ru>
2023-01-19 22:35:41 +0300

    init

---
+++ cezar_code/cezar_code.py
@@ -0,0 +1,4 @@
+eng_lower_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+eng_upper_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+rus_lower_alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+rus_upper_alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
