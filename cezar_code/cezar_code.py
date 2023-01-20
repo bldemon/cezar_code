256f121ef70aac17bd17989b4ff30a0bc82d468a
bldemon <bldemon@yandex.ru>
2023-01-21 01:08:09 +0300

    add def

--- cezar_code/cezar_code.py
+++ cezar_code/cezar_code.py
@@ -1,3 +1,29 @@
+def correct():
+    return
+
+
+def eng_rus():
+    return
+
+
+def encrypt():
+    return
+
+
+def decrypt():
+    return
+
+
+print('Добро пожаловать в программу шифрования.')
+print('В программе используется шифр Цезаря')
+print('Введите текст:')
+text = input()
+print('Для шифрования нажмите 1. Для дешифрования нажмите 2')
+crypt_decrypt = input()
+print('Введите номер сдвига от 1 до 25 для английской фразы и от 1 до 32 для русской фразы:')
+shift = input()
+
+
 eng_lower_alphabet = 'abcdefghijklmnopqrstuvwxyz'
 eng_upper_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 rus_lower_alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
